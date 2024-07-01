c9b8e086553aaf9cf4997857e628b846c9e1a01f
Harshit Pachori <harshitpachori345@gmail.com>
2024-07-01 21:34:06 +0530

    change the server url to production url

--- storypath_frontend/src/appConstant.js
+++ storypath_frontend/src/appConstant.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api/v1";
+// export const BASE_URL = "http://localhost:8080/api/v1";
+export const BASE_URL = "https://storypath-backend-latest.onrender.com/api/v1";
